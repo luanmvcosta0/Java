ebdd883a1e20351d2e4a535e9d395db40fb5f3f2
luanmvcosta0 <luanmvcosta@mgail.com>
2024-09-03 21:12:53 -0300

    Exercicio produtos com desconto em java

---
+++ LogicaJava/src/ProdutoDesconto.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class ProdutoDesconto {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o valor do produto: ");
+        double valorProduto = scanner.nextDouble();
+
+        System.out.print("Digite a quantidade do produto: ");
+        int quantidadeProduto = scanner.nextInt();
+
+        double subtotal = valorProduto * quantidadeProduto;
+
+        boolean quantidadeMaiorOuIgualDez = quantidadeProduto >=10;
+
+        double percentualDesconto = 0.0;
+
+        if (quantidadeMaiorOuIgualDez) {
+            percentualDesconto = 10.0;
+        }
+
+        Double desconto = subtotal * percentualDesconto / 100;
+
+        Double valorTotalDesconto = subtotal - desconto;
+
+        System.out.println("Valor total: " + valorTotalDesconto);
+
+        scanner.close();
+    }
+
+}
