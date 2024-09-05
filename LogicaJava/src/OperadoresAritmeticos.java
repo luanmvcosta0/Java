dd6dd051743cbec016a35cdc8f217bf53b22c728
luanmvcosta0 <luanmvcosta@mgail.com>
2024-09-05 19:07:18 -0300

    Usando operadores aritrimeticos

---
+++ LogicaJava/src/OperadoresAritmeticos.java
@@ -0,0 +1,21 @@
+public class OperadoresAritmeticos {
+
+    public static void main(String[] args) {
+
+        int adicao = 1 + 1;
+        System.out.println("Adição: " + adicao);
+
+        int subtracao = 1 - 1;
+        System.out.println("Subtraão: " + subtracao);
+
+        int multiplicacao = 2 * 2;
+        System.out.println("Multiplicação: " + multiplicacao);
+
+        int divisao = 4 / 2;
+        System.out.println("Divisão: " + divisao);
+
+        int modulo = 7 % 4;
+        System.out.println("Modulo: " + modulo);
+    }
+
+}
