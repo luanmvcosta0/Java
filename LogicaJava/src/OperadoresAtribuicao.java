dd2d329d42872d2f807bab73a0d0e2d9042463bb
luanmvcosta0 <luanmvcosta@mgail.com>
2024-09-09 19:40:36 -0300

    Usando operadores de atribuicao

---
+++ LogicaJava/src/OperadoresAtribuicao.java
@@ -0,0 +1,15 @@
+public class OperadoresAtribuicao {
+
+    public static void main(String[] args) {
+
+        Double numero = 7.0;
+
+        numero = numero - 4;
+
+        System.out.println(numero);
+
+        numero += 4;
+
+    }
+
+}
