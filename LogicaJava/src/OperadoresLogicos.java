643629a001e7eae181057dbbe4940c7687aed7f2
luanmvcosta0 <luanmvcosta@mgail.com>
2024-09-10 19:41:31 -0300

    Fazendo programa pode se aposentar usando operadores logicos

---
+++ LogicaJava/src/OperadoresLogicos.java
@@ -0,0 +1,26 @@
+public class OperadoresLogicos {
+
+    public static void main(String[] args) {
+
+        Boolean carrinhoMaiorQue100 = false;
+        Boolean periodoDePromocao = true;
+        Boolean jaFezCompraNaLoja = false;
+        Boolean pagamentoAVista = true;
+
+        //Boolean aplicarDesconto = carrinhoMaiorQue100 && periodoDePromocao;
+
+        //Boolean aplicarDesconto = carrinhoMaiorQue100 || periodoDePromocao;
+
+        Boolean aplicarDesconto = periodoDePromocao && (carrinhoMaiorQue100 || jaFezCompraNaLoja) && pagamentoAVista;
+
+        if (aplicarDesconto) {
+            System.out.println("Sim, pode aplicar o desconto.");
+        } else {
+            System.out.println("Não aplique o desconto.");
+        }
+
+
+
+    }
+
+}
